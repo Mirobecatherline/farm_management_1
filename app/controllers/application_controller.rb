53724ca381742a3aaacc4b3ccf2920482b2da940
Chrispine Ochieng <chrispineochieng@Chrispines-MacBook-Pro.local>
2023-05-11 23:43:28 +0300

    initial commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,5 @@ end
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password])
     devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :current_password])
   end 
+
 end
